c31b7432b53843f77e2d97c8cb14d5f4d2bf3ed3
dilless <bohuang1994@gmail.com>
2020-07-29 15:47:36 +0800

    hello stage 1

--- hello/hello.go
+++ hello/hello.go
@@ -1,1 +1,15 @@
-package hello
+package main
+
+import "fmt"
+
+const englishHelloPrefix = "Hello, "
+func Hello(name string) string {
+	if name == "" {
+		name = "World"
+	}
+	return englishHelloPrefix + name
+}
+
+func main() {
+	fmt.Println(Hello("world"))
+}
