1b4eccf5e9e1244c66b3ae1d0635da7a42c491fc
dilless <bohuang1994@gmail.com>
2020-07-29 17:23:13 +0800

    sumall

--- array_slice/sum.go
+++ array_slice/sum.go
@@ -2,8 +2,18 @@ package main
 
 func Sum(numbers []int) (sum int) {
 	sum = 0
-	for _, number := range numbers{
+	for _, number := range numbers {
 		sum += number
 	}
 	return sum
+}
+
+func SumAll(numbersToSum ...[]int) (sums []int) {
+	//lengthOfNumbers := len(numbersToSum)
+	//sums = make([]int, lengthOfNumbers)
+
+	for _, numbers := range numbersToSum {
+		sums = append(sums,Sum(numbers))
+	}
+	return sums
 }
