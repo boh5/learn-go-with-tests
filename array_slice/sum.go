57ebaff378be21973f514a92e903a67c1def3616
dilless <bohuang1994@gmail.com>
2020-09-01 15:23:52 +0800

    SumAllTails

--- array_slice/sum.go
+++ array_slice/sum.go
@@ -13,7 +13,21 @@ func SumAll(numbersToSum ...[]int) (sums []int) {
 	//sums = make([]int, lengthOfNumbers)
 
 	for _, numbers := range numbersToSum {
-		sums = append(sums,Sum(numbers))
+		sums = append(sums, Sum(numbers))
 	}
+	return sums
+}
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+
 	return sums
 }
