b6627ecafe7fc82ffa278cc0192005348914d75a
dilless <bohuang1994@gmail.com>
2020-09-02 14:27:19 +0800

    v2

--- mocking/main.go
+++ mocking/main.go
@@ -6,8 +6,16 @@ import (
 	"os"
 )
 
+const (
+	finalWord      = "GO!"
+	countdownStart = 3
+)
+
 func Countdown(out io.Writer) {
-	fmt.Fprint(out, "3")
+	for i := countdownStart; i > 0; i-- {
+		fmt.Fprintln(out, i)
+	}
+	fmt.Fprint(out, finalWord)
 }
 
 func main() {
