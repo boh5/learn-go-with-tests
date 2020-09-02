2c053f1694826a40756cf6e776ab600be3f7ce6b
dilless <bohuang1994@gmail.com>
2020-09-02 15:05:16 +0800

    v3: spySleeper and configurableSleeper

--- mocking/main.go
+++ mocking/main.go
@@ -4,20 +4,44 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"time"
 )
+
+type Sleeper interface {
+	Sleep()
+}
+
+type SpySleeper struct {
+	Calls int
+}
+
+func (s *SpySleeper) Sleep() {
+	s.Calls++
+}
+
+type ConfigurableSleeper struct {
+	duration time.Duration
+}
+
+func (o ConfigurableSleeper) Sleep() {
+	time.Sleep(o.duration)
+}
 
 const (
 	finalWord      = "GO!"
 	countdownStart = 3
 )
 
-func Countdown(out io.Writer) {
+func Countdown(out io.Writer, sleeper Sleeper) {
 	for i := countdownStart; i > 0; i-- {
+		sleeper.Sleep()
 		fmt.Fprintln(out, i)
 	}
+	sleeper.Sleep()
 	fmt.Fprint(out, finalWord)
 }
 
 func main() {
-	Countdown(os.Stdout)
+	sleeper := &ConfigurableSleeper{1 * time.Second}
+	Countdown(os.Stdout, sleeper)
 }
