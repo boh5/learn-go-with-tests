507f21b31ff912c8a85b7d1e89cea33905245aa4
dilless <bohuang1994@gmail.com>
2020-09-07 11:37:45 +0800

    http_server: store score

--- http_server/server.go
+++ http_server/server.go
@@ -13,15 +13,17 @@ type PlayerServer struct {
 	store PlayerStore
 }
 
-
-
 func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
-	if r.Method == http.MethodPost {
-		w.WriteHeader(http.StatusAccepted)
-		return
+	switch r.Method {
+	case http.MethodPost:
+		p.processWin(w)
+	case http.MethodGet:
+		p.showScore(w, r)
 	}
+}
 
+func (p *PlayerServer) showScore(w http.ResponseWriter, r *http.Request) {
 	player := r.URL.Path[len("/players/"):]
 
 	score := p.store.GetPlayerScore(player)
@@ -31,4 +33,8 @@ func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	}
 
 	fmt.Fprint(w, score)
+}
+
+func (p *PlayerServer) processWin(w http.ResponseWriter) {
+	w.WriteHeader(http.StatusAccepted)
 }
